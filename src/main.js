b7920b04729df244f88d1ac6ccdd4beadc548ba4
Maicon Moreno <maiconmoreno@hotmail.com>
2020-05-27 18:10:25 -0300

    Atualizando index e main.js

--- src/main.js
+++ src/main.js
@@ -1,39 +1,114 @@
-import axios from 'axios';
+import api from './api.js';
 
+class App {
 
+    constructor() {
+        this.repositorio = [];
 
-class Api {
+        //elementos html
+        this.elementeForm = document.getElementById('form-repositorio');
+        this.elementoList = document.getElementById('lista-repositorio');
+        this.elementoInput = document.querySelector('input');
 
-    static async getUserFromGitHub(userAcount) {
+        this.registraEvento();
+    }
 
-        try {
+    registraEvento() {
+        this.elementeForm.onsubmit = evento => this.adicionarRepositorio(evento);
+    }
 
-            const response = await axios.get(`https://api.github.com/users/${userAcount}`);
-            console.log(response.data)
+    setLoading(loading = true) {
+        if (loading === true) {
+            let elementoLoading = document.createElement('span');
+            elementoLoading.appendChild(document.createTextNode('Carregando'));
+            elementoLoading.setAttribute('id', 'loading');
 
-        } catch (error) {
-
-            console.log('Usuário não localizado!')
+            this.elementeForm.appendChild(elementoLoading);
+        }
+        else {
+            document.getElementById('loading').remove();
         }
     }
 
-    static async getRepositoriesFromGitHub(repo) {
+    async adicionarRepositorio(evento) {
+        evento.preventDefault();
 
-        try {
+        const repositorioInput = this.elementoInput.value;
 
-            const response = await axios.get(`https://api.github.com/repos/${repo}`);
-            console.log(response.data);
+        if (repositorioInput.length === 0) {
+            alert('Campo vazio, digite um repositorio.');
+            return;
+        }
+        else {
 
-        } catch (error) {
-            console.log('Repositorio não existe')
+            console.log(repositorioInput.length);
+            this.setLoading(true);
+
+            try {
+                const response = await api.get(`/repos/${repositorioInput}`);
+
+                const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+                this.repositorio.push({
+                    avatar_url,
+                    name,
+                    description,
+                    html_url
+                });
+
+                this.renderizar();
+
+                this.elementoInput.value = '';
+
+            } catch (error) {
+
+                alert(`Não encontramos nada com: ${repositorioInput}`)
+                console.warn(error);
+            }
+
+            this.setLoading(false);
         }
     }
-};
-
-// busca dados 
-// Api.getUserFromGitHub('maiconmoreno');
-Api.getRepositoriesFromGitHub('Rocketseat/bootcamp-gostack-desafio-01');
 
 
+    renderizar() {
+
+        this.elementoList.innerHTML = '';
+
+        this.repositorio.forEach(repositorio => {
+
+            // imagem 
+            let elementoImg = document.createElement('img');
+            elementoImg.setAttribute('src', repositorio.avatar_url);
+
+            //Titulo 
+            let elementoTitle = document.createElement('strong');
+            elementoTitle.appendChild(document.createTextNode(repositorio.name));
+
+            //Descrição
+            let elementoDescription = document.createElement('p');
+            elementoDescription.appendChild(document.createTextNode(repositorio.description));
+
+            //html_url
+            let elementoUrl = document.createElement('a');
+            elementoUrl.setAttribute('target', '_black');
+            elementoUrl.setAttribute('href', repositorio.html_url);
+            elementoUrl.appendChild(document.createTextNode('Acessar'));
+
+            // //Adicionando todos componentes na ListaItem
+            let elementoListaItem = document.createElement('li');
+            elementoListaItem.appendChild(elementoImg);
+            elementoListaItem.appendChild(elementoTitle);
+            elementoListaItem.appendChild(elementoDescription);
+            elementoListaItem.appendChild(elementoUrl);
+
+            this.elementoList.appendChild(elementoListaItem);
+
+        });
 
 
+    }
+}
+
+
+new App();
